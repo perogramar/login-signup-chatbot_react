c4710b5f6616f99eec0ee34b1648553a836c63c3
perogramar <nkakkatu@gmail.com>
2024-10-05 16:52:28 +0530

    done

--- src/Components/Pages/LoginSignup.jsx
+++ src/Components/Pages/LoginSignup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginSignup.css';
-import user_icon from '../Assets/person.png';
+
 import email_icon from '../Assets/email.png';
 import password_icon from '../Assets/password.png';
 import logo_icon from '../Assets/logo-ai.png';
